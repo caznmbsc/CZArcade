72a7161e0255ff1c9d1f0544145e24e4b8671e39
mikejemmanuel2 <mikejemmanuel@gmail.com>
2025-08-20 08:15:38 -0700

    Updated JSON script and changed Action to every 3 Hours
    The JSON script now includes an image URL for every game entry that can get one from LaunchBox.
    The Github Action now will only run every 3 hours to compensate for the increased runtime of the script.

--- jsonMaker.py
+++ jsonMaker.py
@@ -2,6 +2,7 @@ from openpyxl import load_workbook
 import json
 import requests
 from io import BytesIO
+from bs4 import BeautifulSoup
 
 print("Retrieving XLSX...")
 sheetsLink = "https://docs.google.com/spreadsheets/d/1Hnb113j20olLsh-SR5zFUsQNV95BMmeXmQOkATtgleI/export?format=xlsx"
@@ -17,16 +18,50 @@ while response.status_code != 200:
 currentWorkbook = load_workbook(filename=BytesIO(response.content), data_only=True)
 print("Downloaded Google Sheets File.\n") 
 
+print("Making JSON...")
+imageDictionary = {}
 data = {}
 for sheetName in currentWorkbook.sheetnames:
+    print("\n-----------------------")
+    print(f"Parsing: {sheetName}")
+    print("-----------------------\n")
     sheet = currentWorkbook[sheetName]
     sheetData = []
     for row in sheet.iter_rows(min_row=1, values_only=False):
         rowData = {}
         for cell in row:
+            if (cell.value == row[0].value) and (cell.value != None):
+                print(cell.value)
             value = cell.value
             if cell.hyperlink:
-                value = {"text": value, "hyperlink": cell.hyperlink.target}
+                imageURL = ""
+                if cell.hyperlink.target in imageDictionary:
+                    imageURL = imageDictionary[cell.hyperlink.target]
+                    print("\tLinked from Cache.")
+                elif cell.hyperlink.target and ("launchbox" in cell.hyperlink.target):
+                    gameDBResponse = requests.get(cell.hyperlink.target)
+                    attempts = 0
+                    while gameDBResponse.status_code != 200:
+                        attempts += 1
+                        if attempts >= 10:
+                            print("Too many Fails. Terminating.")
+                            exit(1)
+                        print("Code: " + str(gameDBResponse.status_code) + ". Failed to Download Google Sheets File. Retrying...")
+                        gameDBResponse = requests.get(sheetsLink)
+                    dbPageHTML = BeautifulSoup(gameDBResponse.text, "html.parser")
+                    if (dbPageHTML.find(string="Screenshot - Gameplay") and
+                        dbPageHTML.find(string="Screenshot - Gameplay").parent.find_next_sibling("div") and
+                        dbPageHTML.find(string="Screenshot - Gameplay").parent.find_next_sibling("div").find("div") and
+                        dbPageHTML.find(string="Screenshot - Gameplay").parent.find_next_sibling("div").find("div").find("img")
+                    ):
+                        imageHolder = dbPageHTML.find(string="Screenshot - Gameplay").parent.find_next_sibling("div")
+                        imageURL = imageHolder.find("div").find("img")["src"]
+                        print("\tFetched Image.")
+                    elif dbPageHTML.find("img"):
+                        imageURL = dbPageHTML.find("img")["src"]
+                        print("\tFetched Image.")
+                    imageDictionary[cell.hyperlink.target] = imageURL
+                value = {"text": value, "hyperlink": cell.hyperlink.target, "imageURL": imageURL}
             rowData[cell.column_letter] = value
         sheetData.append(rowData)
     data[sheetName] = sheetData
